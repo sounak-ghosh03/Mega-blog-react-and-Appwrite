f2ba7d9a82dae01238a039deef85c6ac7e2fee6e
SOUNAK GHOSH <sounak2309@gmail.com>
2025-01-29 21:29:53 +0530

    configuring redux toolkit

---
+++ Mega_BLog_with_Appwrite/src/appwrite/conf.js
@@ -0,0 +1,121 @@
+import config from "../config";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service { 
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            );
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            );
+        } catch (error) {
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries
+            );
+        } catch (error) {
+            return false;
+        }
+    }
+
+    //file upload services
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            );
+        } catch (error) {
+            return false;
+        }
+    }
+
+    async deleteFile(file) {
+        try {
+            return await this.bucket.deleteFileFile(
+                config.appwriteBucketId,
+                fileId
+            );
+        } catch (error) {
+            return false;
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.bucket.getFilePreview(config.appwriteBucketId, fileId);
+    }
+}
+
+const service = new Service();
+export default Service;
---
+++ Mega_BLog_with_Appwrite/src/store/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/tookit";
+
+const store = configureStore({
+    reducer: {},
+});
+
+export default store;
