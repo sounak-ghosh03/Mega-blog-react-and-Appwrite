f5a668aa69fbd9942f94a3976d22bd674708fe26
SOUNAK GHOSH <sounak2309@gmail.com>
2025-02-26 21:21:12 +0530

    debug

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import { AuthLayout, Login } from "./components/index.js";
 import AddPost from "./pages/AddPost";
-import Signup from "./pages/Signup";
+import Signup from "./pages/SignUp.jsx";
 import EditPost from "./pages/EditPost";
 import Post from "./pages/Post";
 import AllPosts from "./pages/AllPosts";
