ccd916e9d89c26116c07c938be96a09a4cc8f66d
SOUNAK GHOSH <sounak2309@gmail.com>
2025-03-02 17:02:47 +0530

    ui improvements

--- src/appwrite/conf.js
+++ src/appwrite/conf.js
@@ -1,7 +1,7 @@
 import config from "../config/config";
 import { Client, ID, Databases, Storage, Query } from "appwrite";
 
-export class Service { 
+export class Service {
     client = new Client();
     databases;
     bucket;
@@ -101,7 +101,7 @@ export class Service {
         }
     }
 
-    async deleteFile(file) {
+    async deleteFile(fileId) {
         try {
             return await this.bucket.deleteFile(
                 config.appwriteBucketId,
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -29,30 +29,22 @@ function Login() {
     return (
         <div className="flex items-center justify-center w-full">
             <div
-                className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+                className={`mx-auto w-full max-w-lg bg-gray-300 rounded-xl p-10 border border-black/10`}
             >
                 <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
                     </span>
                 </div>
-                <h2 className="text-center text-2xl font-bold leading-tight">
+                <h2 className="text-center text-2xl text-black font-bold leading-tight">
                     Sign in to your account
                 </h2>
-                <p className="mt-2 text-center text-base text-black/60">
-                    Don&apos;t have any account?&nbsp;
-                    <Link
-                        to="/signup"
-                        className="font-medium text-primary transition-all duration-200 hover:underline"
-                    >
-                        Sign Up
-                    </Link>
-                </p>
+                
                 {error && (
                     <p className="text-red-600 mt-8 text-center">{error}</p>
                 )}
                 <form onSubmit={handleSubmit(login)} className="mt-8">
-                    <div className="space-y-5">
+                    <div className="space-y-5 text-black/60">
                         <Input
                             label="Email: "
                             placeholder="Enter your email"
@@ -75,10 +67,19 @@ function Login() {
                             {...register("password", { required: true })}
                         />
                     </div>
-                    <Button type="submit" className="w-full">
+                    <Button type="submit" className="w-full m-2">
                         Sign in
                     </Button>
                 </form>
+                <p className="mt-2 text-center text-base text-black/60">
+                    Don&apos;t have any account?&nbsp;
+                    <Link
+                        to="/signup"
+                        className="font-medium text-primary transition-all duration-200 hover:underline"
+                    >
+                        Sign Up
+                    </Link>
+                </p>
             </div>
         </div>
     );
