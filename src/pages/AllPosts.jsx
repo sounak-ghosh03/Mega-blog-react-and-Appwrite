c1d0c24a5fd1ea77d1e58f381bc1f5de639669d6
SOUNAK GHOSH <sounak2309@gmail.com>
2025-03-15 20:58:54 +0530

    debug

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -10,14 +10,13 @@ function AllPosts() {
             setPosts(posts.documents);
         }
     });
-
     return (
         <div className="w-full py-8">
             <Container>
                 <div className="flex flex-wrap">
                     {posts.map((post) => (
                         <div key={post.$id} className="p-2 w-1/4">
-                            <PostCard post={post} />
+                            <PostCard {...post} />
                         </div>
                     ))}
                 </div>
